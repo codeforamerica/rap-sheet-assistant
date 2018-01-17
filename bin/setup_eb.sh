9e9518aca546411f36cf2387c52722239ff356d0
Paras Sanghavi <paras@codeforamerica.org>
2018-01-17 09:55:17 -0800

    Use unix home var
    [#154291830]

--- bin/setup_eb.sh
+++ bin/setup_eb.sh
@@ -3,9 +3,9 @@
 set -x
 set -e
 
-mkdir /home/ubuntu/.aws
-touch /home/ubuntu/.aws/config
-chmod 600 /home/ubuntu/.aws/config
-echo "[profile eb-cli]" > /home/ubuntu/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/.aws/config
+mkdir ~/.aws
+touch ~/.aws/config
+chmod 600 ~/.aws/config
+echo "[profile eb-cli]" > ~/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
