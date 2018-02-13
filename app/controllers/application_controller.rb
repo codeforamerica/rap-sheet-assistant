20eca432b044669d6d3f7a1e26dc441acb9c8191
Travis <travis@TGrathwells-laptop.local>
2018-02-13 12:44:35 -0800

    Require basic auth username and password in production (staging site)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+  end
+
   protect_from_forgery with: :exception
 end
