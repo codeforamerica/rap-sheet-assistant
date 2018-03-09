e959a70af2984cd547a16799d871ee144cc88e29
Paras Sanghavi <paras@codeforamerica.org>
2018-03-09 15:43:45 -0800

    Validate 1-1 relationship between user and financial_information
    [#155267651]
    
    Signed-off-by: Laura Kogler <lkogler@codeforamerica.org>

--- app/models/financial_information.rb
+++ app/models/financial_information.rb
@@ -1,5 +1,7 @@
 class FinancialInformation < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: true
 
   BENEFITS_TYPES = {
     food_stamps: 'Food Stamps',
