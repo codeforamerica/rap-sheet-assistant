8f5d6f398016dc098e39b8911b9e89d3dcbc6d42
Zak Auerbach <zak@codeforamerica.org>
2018-12-17 18:00:24 -0800

    Make adding UUIDs to rap sheet pages irreversible
    Co-authored-by: Laura Kogler <lkogler@codeforamerica.org>

--- db/migrate/20181217215511_add_uuid_to_rap_sheet_pages.rb
+++ db/migrate/20181217215511_add_uuid_to_rap_sheet_pages.rb
@@ -1,10 +1,14 @@
 class AddUuidToRapSheetPages < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :rap_sheet_pages, :uuid, :uuid, default: "gen_random_uuid()", null: false
 
     remove_column :rap_sheet_pages, :id
     rename_column :rap_sheet_pages, :uuid, :id
 
     execute "ALTER TABLE rap_sheet_pages ADD PRIMARY KEY (id);"
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
